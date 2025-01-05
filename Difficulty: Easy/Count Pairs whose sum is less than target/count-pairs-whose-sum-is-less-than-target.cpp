2721788c0790b11f6c3081f680d96a3ee88b5ae6
chaganti uday jaya santhosh <144417335+udayjayasanthosh@users.noreply.github.com>
2025-01-05 14:02:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Pairs whose sum is less than target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Your code here
+        sort(arr.begin(),arr.end());
+        int size=arr.size();
+        int count=0;
+        for(int i=0;i<size-1;i++)
+        {
+            for(int j=i+1;j<size;j++)
+            {
+                if(arr[i]+arr[j]>=target)
+                {
+                    break;
+                }
+                else
+                {
+                    count+=1;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution ob;
+        int res = ob.countPairs(arr, target);
+        cout << res << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
